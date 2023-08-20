9658981a938e202e6588c8a6e498803b034b9c33
jswan <jswannieman@gmail.com>
2023-08-21 10:44:07 +1200

    Commit App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
+import React, { useState, useMemo } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">React useMemo Hook</div>;
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  const [themeChangeCounter, setThemeChangeCounter] = useState(0);
+
+  const calcNumber = useMemo(() => {
+    return slowCalculation(number);
+  }, [number]);
+
+  const themeStyle = useMemo(() => {
+    return {
+      color: dark ? "white" : "navy",
+      backgroundColor: dark ? "navy" : "cyan",
+    };
+  }, [dark]);
+
+  const setDarkTheme = () => {
+    setDark((prevDark) => !prevDark);
+  };
+
+  const handleThemeChangeCounter = () => {
+    setThemeChangeCounter(
+      (prevThemeChangeCounter) => prevThemeChangeCounter + 1
+    );
+  };
+
+  return (
+    <div className="App">
+      <input
+        type="number"
+        value={number}
+        onChange={(event) => setNumber(parseInt(event.target.value))}
+      />
+      <button
+        onClick={() => {
+          setDarkTheme();
+          handleThemeChangeCounter();
+        }}
+      >
+        Change the Theme
+      </button>
+      <h3 style={themeStyle}>{calcNumber}</h3>
+      <h3>Theme has been changed {themeChangeCounter} times</h3>
+    </div>
+  );
 }
 
 export default App;
+
+const slowCalculation = (num) => {
+  for (let i = 0; i < 1000000000; i++) {
+    return num * 5;
+  }
+};
